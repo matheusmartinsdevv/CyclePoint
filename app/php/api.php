7e73d84cc721d13ce075b1571ccb6753702b6341
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-09-29 21:37:17 -0300

    Conexao do bd funcionando

--- app/php/api.php
+++ app/php/api.php
@@ -1,13 +1,28 @@
 <?php
 $conn = mysqli_connect("localhost:3307", "root", "", "db_cyclepoint");
 
-if (["REQUEST_METHOD"] == "GET") {
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $query = mysqli_query($conn, "SELECT * FROM usuario;");
 
     while ($response = mysqli_fetch_assoc($query)) {
-        echo $response["nome"] . $response["cargo"] . "<br>";
+        echo $response["nome"] . "<br>";
     };
 
+};
+
+// CONECTAR COM FORMULARIO DE CADASTRO DE USUARIO/EMPRESA
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // $nome = $_POST['sabor']; 
+    // $valor = $_POST['valor'];
+    // $cor =  $_POST['cor'];
+
+    // $stmt = $conn->prepare("INSERT INTO sabor (nome, valor, cor) values (?,?,?);");
+    // $stmt->bind_param("sds", $nome, $valor, $cor);
+
+    // if ($stmt->execute()) {
+    //     echo 'sabor cadastrado com sucesso';
+    // }
 }
 
 ?>
